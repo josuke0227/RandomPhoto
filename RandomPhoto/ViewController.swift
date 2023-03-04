c618e811727b70f005924de657a84790f2504642
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-03-04 16:35:18 +1100

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by 本杉洋介 on 4/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
